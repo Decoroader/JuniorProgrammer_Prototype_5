ebb5d4bad83a1315105d6294ec514c4e2554992d
Decoroader <secbittree@gmail.com>
2021-01-17 21:17:09 +0200

    5.Create a new UpdateScore method
    4.Initialize score text and variable, 5.Create a new UpdateScore method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
 
     //private float spawnRate;
 
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+    }
+
+    private void UpdateScore(int scoreToAdd)
+	{
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
     IEnumerator SpawnTarget()
 	{
@@ -18,6 +28,8 @@ public class GameManager : MonoBehaviour
 		{
             yield return new WaitForSeconds(1);
             Instantiate(targets[Random.Range(0, targets.Count)]);
-		}
+            UpdateScore(5);
+
+        }
 	}
 }
