3f7b87daddf3ecbc923ba2d7aa46633cb62a9780
Decoroader <secbittree@gmail.com>
2021-01-18 19:41:00 +0200

    Lesson 5.2 - Keeping Score
    8.Add a Particle explosion

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public int pointValue;
+    public ParticleSystem explosionParticle;
 
     private GameManager gameManager;
     private Rigidbody targetRb;
@@ -37,6 +38,7 @@ public class Target : MonoBehaviour
 	{
         Destroy(gameObject);
         gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     }
     private void OnTriggerEnter(Collider other)
 	{
